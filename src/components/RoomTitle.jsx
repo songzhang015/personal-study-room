ad21f84e73488be22a1c8998ffb7bea724243299
Song <songzhang015@gmail.com>
2025-09-14 15:36:06 -0700

    Allow page title/header to be edited and add checks to prevent leading spaces and zero characters

--- src/components/RoomTitle.jsx
+++ src/components/RoomTitle.jsx
@@ -1,9 +1,35 @@
 import "../styles/RoomTitle.css";
+import { useState } from "react";
 
 function RoomTitle() {
+	const defaultTitle = "Personal Study Room";
+	const [title, setTitle] = useState(defaultTitle);
+	const [isEditing, setIsEditing] = useState(false);
+	const handleBlur = () => {
+		let newTitle = title.replace(/^\s+/, "").replace(/\s+$/, "");
+		if (newTitle.length === 0) newTitle = defaultTitle;
+		setTitle(newTitle);
+		setIsEditing(false);
+	};
+
 	return (
 		<div className="room-title">
-			<h1>Personal Study Room</h1>
+			{isEditing ? (
+				<input
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+					onBlur={handleBlur}
+					size={title.length}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							handleBlur();
+						}
+					}}
+					autoFocus
+				/>
+			) : (
+				<h1 onClick={() => setIsEditing(true)}>{title}</h1>
+			)}
 		</div>
 	);
 }
